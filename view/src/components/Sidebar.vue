18a14f4c225b16346206bc9475a4f2e951a8ce5a
Luiz Henrique <43296232+henriquesaydt@users.noreply.github.com>
2021-03-25 16:56:22 -0300

    daily update

--- view/src/components/Sidebar.vue
+++ view/src/components/Sidebar.vue
@@ -2,45 +2,67 @@
     <div>
         <b-sidebar ref="sidebar" id="sidebarID" title="Configurações" backdrop backdrop-variant="" shadow>
             <div class="px-3 py-2">
-                <b-row>
-                    <b-col cols="3">
-                        <b-form-group label-cols="3" style="font-weight: bold" label="Status:" label-for="status"/>
-                    </b-col>
-                    <b-col cols="1">
-                        <font-awesome-icon class="text-danger" style="margin-top: 12px" icon="circle"/>
-                    </b-col>
-                    <b-col cols="7" class="text-left">
-                        <h6 style="margin-top: 9px"><b>Desconectado</b></h6>
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col cols="8"> 
-                        <b-form-group style="font-weight: bold" label="Endereço:" label-for="ip">
-                            <b-input id="ip"/>
-                        </b-form-group>
-                    </b-col>
-                    <b-col cols="4"> 
-                        <b-form-group style="font-weight: bold" label="Porta:" label-for="porta">
-                            <b-input id="porta"/>
-                        </b-form-group>
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col cols="12">
-                        <b-form-group style="font-weight: bold" label="Token:" label-for="token">
-                            <b-input id="token" type="password"/>
-                        </b-form-group>
-                    </b-col>
-                </b-row>
-                <b-row class="mt-3">
-                    <b-col class="text-center" cols="6">
-                        <b-button variant="danger">Desconectar</b-button>
-                    </b-col>
-                    <b-col class="text-center" cols="6">
-                        <b-button variant="success">Conectar</b-button>
-                    </b-col>
-                </b-row>
+                <b-form method="POST" ref="formConfig">
+                    <b-row>
+                        <b-col cols="3">
+                            <b-form-group label-cols="3" style="font-weight: bold" label="Status:" label-for="status"/>
+                        </b-col>
+                        <b-col cols="1">
+                            <font-awesome-icon class="text-danger" style="margin-top: 12px" icon="circle"/>
+                        </b-col>
+                        <b-col cols="7" class="text-left">
+                            <h6 style="margin-top: 9px"><b>Desconectado</b></h6>
+                        </b-col>
+                    </b-row>
+                    <b-row>
+                        <b-col cols="8"> 
+                            <b-form-group style="font-weight: bold" label="Endereço:" label-for="ip">
+                                <b-input id="ip" name="ip"/>
+                            </b-form-group>
+                        </b-col>
+                        <b-col cols="4"> 
+                            <b-form-group style="font-weight: bold" label="Porta:" label-for="porta">
+                                <b-input id="port" type="number" name="port"/>
+                            </b-form-group>
+                        </b-col>
+                    </b-row>
+                    <b-row>
+                        <b-col cols="12">
+                            <b-form-group style="font-weight: bold" label="Token:" label-for="token">
+                                <b-input id="token" name="token" type="password"/>
+                            </b-form-group>
+                        </b-col>
+                    </b-row>
+                    <b-row class="mt-3">
+                        <b-col class="text-center" cols="6">
+                            <b-button variant="danger">Desconectar</b-button>
+                        </b-col>
+                        <b-col class="text-center" cols="6">
+                            <b-button @click="conect" type="submit" variant="success">Conectar</b-button>
+                        </b-col>
+                    </b-row>
+                </b-form>
             </div>
         </b-sidebar>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+        conect() {
+            const form = new FormData(this.$refs.formConfig);
+            this.$store.commit('setConfig', {ip: form.get('ip'), port: form.get('port'), token: form.get('token')});
+        },
+        desconect() {
+            this.$store.commit('desconect');
+        }
+    }
+}
+</script>
--- view/src/main.js
+++ view/src/main.js
@@ -3,6 +3,7 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
@@ -20,13 +21,43 @@ library.add(faUpload)
 library.add(faBars)
 library.add(faCircle)
 
+Vue.use(Vuex)
+
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  state: {
+    config: {
+      ip: "",
+      port: "",
+      token: ""
+    }
+  },
+  mutations: {
+    setConfig(state, {ip, port, token}) {
+      state.config = {
+        ip: ip,
+        port: port,
+        token: token
+      }
+      
+    },
+    desconect(state) {
+      state.config = {
+        ip: "",
+        port: "",
+        token: ""
+      }
+    }
+  }
+})
 
 new Vue({
   render: h => h(App),
   components: {
     FontAwesomeIcon
-  }
+  },
+  store: store
 }).$mount('#app')
